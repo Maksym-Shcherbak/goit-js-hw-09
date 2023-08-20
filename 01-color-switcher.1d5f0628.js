6ab80f38ae5f3a2fc5a97e15c59c58c93f6c34fd
Maksym-Shcherbak <130525995+Maksym-Shcherbak@users.noreply.github.com>
2023-08-20 20:38:10 +0000

    Deploying to gh-pages from @ Maksym-Shcherbak/goit-js-hw-09@9aeac1e2ecebfb37e083d3c94607bc79cf0e6897 🚀

---
+++ 01-color-switcher.1d5f0628.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.querySelector("body"),o=null;function c(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){e.style.backgroundColor="".concat(c()),o=setInterval((function(){e.style.backgroundColor="".concat(c())}),1e3),t.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.1d5f0628.js.map
